775c36af75e32f6c0083f35c1909f28421ab26d9
Togosling <toga.abdyldaev@gmail.com>
2023-12-12 15:58:41 +0600

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Тагай Абдылдаев on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
